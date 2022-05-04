1e6fd3c910a81c22d6181cb76f0c70e1ebbbbab0
Safwaan <hellosafwaan@gmail.com>
2022-05-04 23:58:06 +0530

    new function - journeyDetails()

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+s = Service(PATH)
+driver = webdriver.Chrome(service = s)
+
+def journeyDetails(trainNum, stationName, jDate) -> None:
+    print(f'collecting data for {trainNum}')
+    driver.find_element(By.XPATH, xpaths['trainNoInput']).send_keys(trainNum, Keys.RETURN)
+    station = driver.find_element(By.XPATH, xpaths['boardingStationInput'])
+    station.send_keys(stationName)
+    station.send_keys(Keys.RETURN)
+    station.send_keys(Keys.RETURN)
+    select_date(jDate)
+    driver.find_element(By.XPATH, xpaths['trainChartButton']).click()
