941abb6f168f347710b6d24a6f0089b028cbce98
Safwaan <hellosafwaan@gmail.com>
2022-05-05 21:03:18 +0530

    new function - select_date()

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ def journey_details(train_num, station_name, j_date) -> None:
     station.send_keys(Keys.RETURN)
     select_date(j_date)
     driver.find_element(By.XPATH, xpaths['train_chart_button']).click()
+    
+def select_date(journey_date) -> None:
+    journey_day = journey_date[8:10]
+    if journey_day[0] == '0':
+        journey_day = journey_day[1]
+    driver.find_element(By.XPATH, xpaths['journey_date_input']).click()
+    dates = driver.find_elements(By.XPATH, xpaths['date_buttons'])
+    for d_elem in dates:
+        if d_elem.text == journey_day and d_elem.get_attribute('tabindex') == '0':
+            d_elem.click()
+            break
