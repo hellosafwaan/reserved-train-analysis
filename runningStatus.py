88ccb59f1dcecbb85584a3a432314c367d1fdf6e
Safwaan <hellosafwaan@gmail.com>
2022-05-10 22:33:29 +0530

    new function runningStatus.train_details();

--- runningStatus.py
+++ runningStatus.py
@@ -14,3 +14,14 @@ driver = webdriver.Chrome(service = s)
 driver.get('https://enquiry.indianrail.gov.in/mntes/')
 parent_window = driver.current_window_handle
 driver.implicitly_wait(5)
+
+def train_details(train_num):
+    train_num_elem = driver.find_element(By.ID, 'trainNo')
+    train_num_elem.send_keys(train_num)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="jStation"]/option[2]')))
+    Select(driver.find_element(By.ID, 'jStation')).select_by_index('1')
+    prev_day = driver.find_element(By.ID, "jYesterday")
+    try:
+        prev_day.click()
+    except StaleElementReferenceException as e:
+        prev_day = driver.find_element(By.ID, "jYesterday").click()
