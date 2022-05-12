01a52f5b6e614efc4749e2102b3b72ce4b85f576
Safwaan <hellosafwaan@gmail.com>
2022-05-12 21:57:32 +0530

    added xpaths files

---
+++ xpaths.py
@@ -0,0 +1,19 @@
+xpaths = {
+    'train_no_input': '/html/body/div[1]/div/div/div[1]/div/div/div/div/label[1]/div/div/div[1]/div[1]/div/div/input',
+    'boarding_station_input': '/html/body/div[1]/div/div/div[1]/div/div/div/div/label[2]/div/div/div[1]/div[1]/div/div/input',
+    'journey_date_input': '/html/body/div[1]/div/div/div[1]/div/div/div/div/div[1]/div/div/input',
+    'date_buttons': '/html/body/div[2]/div[2]/div/div[1]/div[3]/div/div/div/button',
+    'train_chart_button': '/html/body/div[1]/div/div/div[1]/div/div/div/div/div[2]/button',
+    'general_coaches_button':'/html/body/div[1]/div/div/div[2]/div[1]/div/div/div/div/button',
+    'iterable_coaches_buttons':'/html/body/div[1]/div/div/div[2]/div[1]/div/div/div/div[{}]/button', # For 2nd Chart Starts from 2
+    'coach_sort_button': '/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div/table/thead/tr/th[3]/span',
+    'noOfRows_option_div': '/html/body/div[1]/div/div/div[2]/div/div/div[1]/table/tfoot/tr/td/div/div[2]/div/div',
+    'select_50Rows': '/html/body/div[3]/div[2]/ul/li[3]',
+    'page_details': '/html/body/div[1]/div/div/div[2]/div/div/div[1]/table/tfoot/tr/td/div/span[2]',
+    'next_page_button': '/html/body/div[1]/div/div/div[2]/div/div/div[1]/table/tfoot/tr/td/div/div[3]/span[3]/button',
+    'table_data': '/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div/table/tbody/tr[{}]/td[{}]',
+    'back_button': '/html/body/div[1]/div/header[2]/div/button[2]',  
+    'headers_data': '/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div/table/thead/tr/th[{}]/span/div'
+}
+
+
